ebf009993963cdbf76e18a25c255d05bba6a26df
cpouthier <cpouthier@me.com>
2023-08-28 10:09:32 +0200

    Review

--- installscript.sh
+++ installscript.sh
@@ -1,34 +1,41 @@
 #! /bin/bash
 ##new version of the script
 # the following command will set the ubuntu service restart under apt to automatic
+
 sed -i 's/#$nrconf{restart} = '"'"'i'"'"';/$nrconf{restart} = '"'"'a'"'"';/g' /etc/needrestart/needrestart.conf
 echo -e "$G Installing pre-req's...please standby..."
 sleep 10
 apt update
-apt -qq install apache2-utils ruby-rubygems -y
-gem install facter
+# apt -qq install apache2-utils ruby-rubygems -y --> do we need apache 2 to be installed for K10??
 
+
+
+#Setting up interactively some environment variables to run this script
+echo "Kasten will be installed with basic authentication, hence the need to provide a username and a password."
+echo "You will use also those credentials to connect to Minio."
+echo "Enter the username: "
+read username < /dev/tty
+echo "Enter the password: "
+read password < /dev/tty
+htpasswd_entry=$(htpasswd -nbm "$username" "$password" | cut -d ":" -f 2)
+htpasswd="$username:$htpasswd_entry"
+echo "Successfully generated htpasswd entry: $htpasswd"
+echo "Please wait..."
+sleep 5
+fdisk -l
 echo ""
-echo -e "$G This will install a single node k3s cluster with the OpenEBS ZFS csi driver, Longhorn csi driver and all k10 annotated volumesnapshotclasses"
-sleep 1
-echo ""
-echo -e "$G Longhorn will operate in single REPLICA mode, so this cluster cannot have additional worker nodes added to it"
-sleep 1
-echo ""
-echo -e "$G It will then install k10 via HELM and automatically expose the k10 dashboard on the cluster load balancer"
-sleep 1
-echo ""
-echo -e "$G K10 will be install with Basic Authentication enabled and you need to enter the username and password you wish to use next: "
-sleep 1
-echo -e "$W"
-echo ""
-echo -e "$G Enter drive path of extra volume (ie /dev/sdb). If you do not know this exit this script by cmd-x and run "fdisk -l" to find the drive path: "
-echo -e "$W "
+echo " Enter drive path of extra volume (ie /dev/sdb) to set up Kasten K10 zfs pool: "
 read DRIVE < /dev/tty
- 
+
+#Install curl and wget if not present
 pro config set apt_news=false
 apt -qq update && apt -qq upgrade -y && apt -qq dist-upgrade -y && apt -qq autoremove -y
 sudo apt install -y curl wget
+
+#Install Helm
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod +x ./get_helm.sh
+./get_helm.sh
 
 #Installing k3s single node cluster with local storage disabled 
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--disable local-storage" sh -s -
@@ -42,83 +49,7 @@ k3s check-config
 kubectl cluster-info
 kubectl get nodes -o wide
 
-#Install zfs and configure kasten-pool storage pool on associated drive
-apt install zfsutils-linux open-iscsi jq -y
-zpool create kasten-pool $DRIVE
-
-#Configure zfs storage class
-kubectl apply -f https://openebs.github.io/charts/zfs-operator.yaml
-#curl -s https://raw.githubusercontent.com/jdtate101/jdtate101/main/zfs-sc.yaml > zfs-sc.yaml
-#curl -s https://raw.githubusercontent.com/jdtate101/jdtate101/main/zfs-snapclass.yaml > zfs-snapclass.yaml
-kubectl apply -f zfs-sc.yaml
-kubectl apply -f zfs-snapclass.yaml
-kubectl patch storageclass kasten-zfs -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
-sleep 5
-echo ""
-echo -e "$G Installing Helm"
-echo -e "$W "
-sleep 5
-curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
-chmod +x ./get_helm.sh
-./get_helm.sh
-
-#Adding kubectl autocompletion
-echo 'source <(kubectl completion bash)' >>~/.bashrc
-source <(kubectl completion bash)
-
-sleep 5
-echo ""
-echo -e "$G Installing Longhorn Storage & VolumeSnapshotClass"
-helm repo add longhorn https://charts.longhorn.io
-helm repo update
-helm install longhorn longhorn/longhorn --namespace longhorn-system --create-namespace -f https://raw.githubusercontent.com/jdtate101/jdtate101/main/longhorn-values.yaml
-curl -s https://raw.githubusercontent.com/jdtate101/jdtate101/main/longsnapclass.yaml > longsnapclass.yaml
-kubectl apply -f longsnapclass.yaml
-
-
-echo -e "$G Installing Kasten K10"
-helm repo add kasten https://charts.kasten.io
-helm repo update
-echo "Enter the username: "
-read username < /dev/tty
-echo ""
-echo "Enter the password: "
-read password < /dev/tty
-htpasswd_entry=$(htpasswd -nbm "$username" "$password" | cut -d ":" -f 2)
-htpasswd="$username:$htpasswd_entry"
-echo "Successfully generated htpasswd entry: $htpasswd"
-sysctl fs.inotify.max_user_watches=524288
-sysctl fs.inotify.max_user_instances=512
-echo "fs.inotify.max_user_watches = 524288" >> /etc/sysctl.conf
-echo "fs.inotify.max_user_instances = 512" >> /etc/sysctl.conf
-kubectl create ns kasten-io
-helm install k10 kasten/k10 --namespace kasten-io --set "auth.basicAuth.enabled=true" --set auth.basicAuth.htpasswd=$htpasswd
-echo ""
-echo -e "$R Please wait for 60sec whilst we wait for the pods to spin up..."
-echo -e "$R After this period the external URL for K10 access will display (DO NOT exit this script)"
-sleep 60
-echo -e "$W "
-pod=$(kubectl get po -n kasten-io |grep gateway | awk '{print $1}' )
-kubectl expose po $pod -n kasten-io --type=LoadBalancer --port=8000 --name=k10-dashboard
-port=$(kubectl get svc -n kasten-io |grep k10-dashboard | cut -d':' -f2- | cut -f1 -d'/' )
-curl https://raw.githubusercontent.com/jdtate101/jdtate101/main/kasten-ingress.yaml > kasten-ingress.yaml
-kubectl apply -f kasten-ingress.yaml -n kasten-io
-#OK SO FAR!!!!!!!!!!!!!!!!!!!!!!
-#get_public_ip=$(curl -s ifconfig.me) #ifconfig deprecated
-#get_local_ip=$(hostname -I | awk '{print $1}')
-#cloud_id=$(facter cloud |grep provider | cut -d'"' -f 2)
- #   if [[ ! -z $cloud_id ]]; then
- #      echo "Running on a cloud virtual machine"
- #     echo -e "$G K10 dashboard can be accessed on http://"$get_public_ip":"$port"/k10/#/" 
- #else
-#   echo "Running on a local machine / virtual machine"
-#   echo -e "$G K10 dashboard can be accessed on http://"$get_local_ip":"$port"/k10/#/"
-#   fi
-
-
-
-echo "Installing minio S3 Storage.."
-echo -e "$W"
+#Installing Minio
 wget https://dl.min.io/server/minio/release/linux-amd64/minio -P /root
 chmod +x /root/minio
 mv /root/minio /usr/local/bin
@@ -126,27 +57,83 @@ mkdir /minio
 MINIO_ROOT_USER=$username MINIO_ROOT_PASSWORD=$password minio server /minio --console-address ":9001" &
 echo "@reboot MINIO_ROOT_USER=$username MINIO_ROOT_PASSWORD=$password minio server /minio --console-address ":9001"" > /root/minio_cron
 crontab /root/minio_cron
-echo -e "$G"
-echo "Minio console is available on port 9001 for the same IP address as the K10 interface (listed above), with the same username/password you set for the K10 instance, and the API available on port 9000"
-sleep 2
+get_ip=$(hostname -I | awk '{print $1}')
+echo "Minio console is available on  http://"$get_ip":9001, with the same username/password you set, and the API available on port 9000."
+echo "Please wait 10 sec..."
+sleep 10
+
+
+
+#Install zfs and configure kasten-pool storage pool on associated drive
+apt install zfsutils-linux open-iscsi jq -y
+zpool create kasten-pool $DRIVE
+#
+#Configure zfs storage class
+kubectl apply -f https://openebs.github.io/charts/zfs-operator.yaml
+curl -s https://raw.githubusercontent.com/cpouthier/ubuntu-k3s-minio-K10/main/k10-zfs-sc.yaml > zfs-sc.yaml
+curl -s https://raw.githubusercontent.com/cpouthier/ubuntu-k3s-minio-K10/main/k10-zfs-snapshotclass.yaml > zfs-snapclass.yaml
+kubectl apply -f zfs-sc.yaml
+kubectl apply -f zfs-snapclass.yaml
+kubectl patch storageclass kasten-zfs -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
+#
+#Adding kubectl autocompletion to bash
+echo 'source <(kubectl completion bash)' >>~/.bashrc
+source <(kubectl completion bash)
+#
+#Install Longhorn Storage & VolumeSnapshotClass
+echo -e "$G Installing Longhorn Storage & VolumeSnapshotClass"
+helm repo add longhorn https://charts.longhorn.io
+helm repo update
+helm install longhorn longhorn/longhorn --namespace longhorn-system --create-namespace -f https://raw.githubusercontent.com/cpouthier/ubuntu-k3s-minio-K10/main/longhorn-values.yaml
+curl -s https://raw.githubusercontent.com/cpouthier/ubuntu-k3s-minio-K10/main/longhorn-snapshotclass.yaml > longsnapclass.yaml
+kubectl apply -f longsnapclass.yaml
+#
+#Install Kasten K10
+#Run Kasten k10 primer
+curl https://docs.kasten.io/tools/k10_primer.sh | bash
+#
+#Adding and updating Helm repository
+helm repo add kasten https://charts.kasten.io
+helm repo update
+#
+#What is this used for?
+#sysctl fs.inotify.max_user_watches=524288
+#sysctl fs.inotify.max_user_instances=512
+#echo "fs.inotify.max_user_watches = 524288" >> /etc/sysctl.conf
+#echo "fs.inotify.max_user_instances = 512" >> /etc/sysctl.conf
+#
+#Create kasten-io namespace
+kubectl create ns kasten-io
+#Install Kasten in the kasten-io namespace with basic authentication
+helm install k10 kasten/k10 --namespace kasten-io --set "auth.basicAuth.enabled=true" --set auth.basicAuth.htpasswd=$htpasswd
 echo ""
+echo "Please wait for 60sec whilst we wait for the pods to spin up..."
+echo "After this period the external URL for K10 access will display (DO NOT exit this script)"
+sleep 60
+echo ""
+#Finding the Kasten K10 gateway namespace name
+pod=$(kubectl get po -n kasten-io |grep gateway | awk '{print $1}' )
+#Expose the gateway pod through the load balancer on port 8000
+kubectl expose po $pod -n kasten-io --type=LoadBalancer --port=8000 --name=k10-dashboard
+#Setting up Kasten k10 ingress
+curl https://raw.githubusercontent.com/cpouthier/ubuntu-k3s-minio-K10/main/k10-ingress.yaml > kasten-ingress.yaml
+kubectl apply -f kasten-ingress.yaml -n kasten-io
+echo "Kasten k10 is installed and can be accessed on http://"$get_ip":8000/k10/#/ using credentials set up earlier in this script" 
 
 
-
-
-
-
-echo "Now deploying sample pacman application..."
-echo -e "$W"
+#Optional: deploying a sample pacman application
 kubectl create ns pacman
 helm repo add pacman https://shuguet.github.io/pacman/
 helm install pacman pacman/pacman -n pacman
-echo -e "$R"
-echo "Waiting for pacman app to become available..please wait!"
+echo "Please wait 5sec for the pacman app to spin up..."
 sleep 5
 curl https://raw.githubusercontent.com/jdtate101/jdtate101/main/pacman-ingress.yaml > pacman-ingress.yaml
 kubectl apply -f pacman-ingress.yaml -n pacman
-kubectl expose po pacman-844b99555f-kj8rd  -n pacman --type=LoadBalancer --port=8001 --name=pacmanweb
+#Finding the Kasten K10 gateway namespace name
+pod=$(kubectl get po -n pacman |grep gateway | awk '{print $1}' )
+
+
+kubectl get po pacman-844b99555f-rt5m2 -n pacman --type=LoadBalancer --port 8001 --name=pacmanwebui
 
 echo -e "$G"
 echo ""
