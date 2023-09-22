90d64a403d052f9b9831e9632b0961391e6393b0
cpouthier <80251456+cpouthier@users.noreply.github.com>
2023-09-22 14:25:06 +0200

    Update

---
+++ extra_tools/docker_install.sh
@@ -0,0 +1,8 @@
+#Install Docker on Ubuntu
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+#Install Portainer in Docker
+docker volume create portainer_data
+#Run Portainer with automatic restart and expose port 8000
+docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+#Install portainer agent in Kubernetes
+kubectl apply -f https://downloads.portainer.io/ce2-19/portainer-agent-k8s-nodeport.yaml
