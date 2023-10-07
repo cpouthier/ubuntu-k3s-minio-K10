c948804aaa0a30741a9ba5e97f9488d6a27ed3d8
cpouthier <80251456+cpouthier@users.noreply.github.com>
2023-10-07 11:18:31 +0200

    Update portainer-install.sh

--- extra_tools/portainer-install.sh
+++ extra_tools/portainer-install.sh
@@ -1,3 +1,4 @@
 helm repo add portainer https://portainer.github.io/k8s/
 helm repo update
+#by default expose through https on port 30779
 helm upgrade --install --create-namespace -n portainer portainer portainer/portainer --set tls.force=true
